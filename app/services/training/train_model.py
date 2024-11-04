5ae7c38c07fb6fda2b810e13df3787e298d83508
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-04 10:20:52 +0100

    # Commit message for train_model.py # Add documentation comments to the 'train_model' function # Explain the purpose, parameters, and return values of the function # Improve code documentation and maintainability # Add a new function 'cross_validate_model' to perform cross-validation on the trained model # Explain the purpose, parameters, and return values of the new function # Improve code documentation and maintainability # Add a new function 'save_model' to save the trained model and scaler to a specified directory # Explain the purpose, parameters, and return values of the new function # Improve code documentation and maintainability # Add a new function 'prepare_and_train_model' to prepare and train a machine learning model for predicting NutriScore # Explain the purpose, parameters, and return values of the new function # Improve code documentation and maintainability

--- app/services/training/train_model.py
+++ app/services/training/train_model.py
@@ -124,26 +124,6 @@ def train_model(x_for_train, y_for_train):
     model.fit(x_for_train, y_for_train)
     return model, std_scaler
 
-
-def make_test_predictions(model, std_scaler, x_for_test):
-    """
-    Make predictions using a trained logistic regression model and a scaler.
-
-    Parameters:
-    log_reg_model (sklearn.linear_model.LogisticRegression): The trained logistic regression model.
-    std_scaler (sklearn.preprocessing.StandardScaler): The scaler used to transform the input data.
-    x_for_test (pd.DataFrame): A pandas DataFrame containing the features for the test set.
-
-    Returns:
-    y_prediction_test (pd.Series): A pandas Series containing the predicted target values for the test set.
-    y_prediction_prob_test (np.ndarray): A numpy array containing the predicted probabilities for each class.
-    """
-    x_for_test = std_scaler.transform(x_for_test)
-    y_prediction_test = model.predict(x_for_test)
-    y_prediction_prob_test = model.predict_proba(x_for_test)
-    return y_prediction_test, y_prediction_prob_test
-
-
 def save_model(model, scaler):
     """
     Saves the trained model and scaler to a specified directory.
