46233fdbc6b94d6e2cb7d9f608cc7c7f1e0863a2
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-04 10:19:03 +0100

    # Commit message for predictions_tests.py # Create a main function to load data, make predictions, calculate accuracy, and display the report # Import the necessary functions from the app.services.testing package # Import the configuration settings from the config module # Call the main function when the script is executed # Improve code documentation and maintainability

--- scripts/predictions_tests.py
+++ scripts/predictions_tests.py
@@ -4,17 +4,16 @@ from app.services.testing.test_model import (load_data_for_test,
                                              make_predictions,
                                              calculate_accuracy,
                                              get_accuracy_report)
+from config import Config
 
 
 def main():
     """
     Main function to load data, make predictions, calculate accuracy, and display the report.
     """
-    model_path = 'models/model_nutriscore.pkl'
-    scaler_path = 'models/scaler_nutriscore.pkl'
-    test_data_path = 'tests/df_test_nutriscore.pkl'
-
-    model, scaler, df_test = load_data_for_test(model_path, scaler_path, test_data_path)
+    model, scaler, df_test = load_data_for_test(Config.DUMP_MODEL_PATH,
+                                                Config.DUMP_SCALER_PATH,
+                                                Config.DUMP_TEST_PATH)
     df_test = make_predictions(model, scaler, df_test)
     display_predictions(df_test)
     correct_predictions, total_predictions, accuracy_percentage = calculate_accuracy(df_test)
