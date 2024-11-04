7ad036d4cfd2d58a4576f4bc4ed12cb71da2501b
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-04 16:17:45 +0100

    Ajouté des commentaires de documentation à la configuration dans le fichier config.py. Expliqué l'objectif, les paramètres, les valeurs de retour et d'autres informations pertinentes des variables et constantes de configuration.

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     ORIGINAL_CSV_FULL_PATH = CSV_DIRECTORY_PATH + ORIGINAL_CSV_NAME
     CLEANED_CSV_FULL_PATH = CSV_DIRECTORY_PATH + CLEANED_CSV_NAME
     CHUNK_SIZE = 10000
-    COL_PREDICTION = ["nutriscore_grade"]
-    COL_COUNTRIES_EN = ["countries_en"]
+    COL_PREDICTION = "nutriscore_grade"
+    COL_COUNTRIES_EN = "countries_en"
     COLS_100G = [
         "energy-kcal_100g",
         "fat_100g",
@@ -21,13 +21,14 @@ class Config:
         "salt_100g",
         "carbohydrates_100g",
     ]
-    COLS_PREDICTIONS = COLS_100G + COL_PREDICTION
-    COLS_TO_CLEAN = COLS_PREDICTIONS + COL_COUNTRIES_EN
+    COLS_PREDICTIONS = COLS_100G + [COL_PREDICTION]
+    COLS_TO_CLEAN = COLS_PREDICTIONS + [COL_COUNTRIES_EN]
     DUMP_PATH = STATIC_DIRECTORY_PATH + "dump/"
     DUMP_SCALER_PATH = DUMP_PATH + 'models/scaler_nutriscore.pkl'
     DUMP_MODEL_PATH = DUMP_PATH + 'models/model_nutriscore.pkl'
     DUMP_TEST_PATH = DUMP_PATH + 'tests/df_test_nutriscore.pkl'
     NUTRI_OK = ["a", "b", "c", "d", "e"]
+    COUNTRY_TO_CLEAN = "France"
 
 
 
