c6473e2e94acca95028b666ef240df14e1e3a08b
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-05 09:28:48 +0100

    Déplacement du fichier

--- scripts/csv/clean_csv.py
+++ scripts/csv/clean_csv.py
@@ -1,8 +1,6 @@
-import pandas as pd
-
 from app.services.cleaning.cleaners import clean_nutriscore, clean_by_country
 from app.services.csv.csv import get_df_from_csv
-from config import Config
+from app.config import Config
 
 def main():
     """
@@ -16,17 +14,19 @@ def main():
     --------
     None
     """
+    print("Début de script clean_csv")
     df_to_clean = get_df_from_csv(Config.ORIGINAL_CSV_FULL_PATH,
                                   Config.COLS_TO_CLEAN,
                                   Config.CHUNK_SIZE)
-    df_cleaned = df_to_clean.dropna()
-    df_cleaned = clean_nutriscore(df_cleaned, Config.COL_PREDICTION)
+    df_cleaned = clean_nutriscore(df_to_clean, Config.COL_PREDICTION)
     df_cleaned = clean_by_country(df_cleaned,
                                   Config.COL_COUNTRIES_EN,
                                   Config.COUNTRY_TO_CLEAN)
     df_cleaned = df_cleaned.drop(Config.COL_COUNTRIES_EN, axis=1)
     # Save the cleaned DataFrame to a new CSV file with tab-separated values and without the index column
     df_cleaned.to_csv(Config.CLEANED_CSV_FULL_PATH, sep='\t', index=False)
+    print("CSV sauvegardé")
+    print("Fin de script clean_csv")
 
 # Call the main function
 if __name__ == "__main__":
